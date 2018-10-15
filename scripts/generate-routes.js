46a869d283364d0f19e73f52d51c39fe35b8807f
MunifTanjim <muniftanjim@gmail.com>
2018-10-15 19:41:10 +0600

    add secret.databases & secret.databases.mysql endpoints

--- scripts/generate-routes.js
+++ scripts/generate-routes.js
@@ -5,7 +5,9 @@ const {
   resolve: resolvePath
 } = require('path')
 const { writeFile } = require('fs-extra')
-const { camelCase, set } = require('lodash')
+const camelCase = require('lodash/camelCase')
+const defaultsDeep = require('lodash/defaultsDeep')
+const set = require('lodash/set')
 const recursiveReadDir = require('recursive-readdir')
 
 const deepClean = require('clean-deep')
@@ -14,30 +16,92 @@ const deepSort = require('deep-sort-object')
 const ENDPOINTS_DIR = resolvePath('endpoints')
 const ROUTES_DIR = resolvePath('src/routes')
 
+const processParameters = parameters =>
+  parameters.reduce((params, { description, name, required, ...param }) => {
+    if (required) {
+      param.required = true
+    }
+
+    if (param.type === 'object' && param.properties) {
+      Object.keys(param.properties).forEach(key => {
+        delete param.properties[key].description
+      })
+    }
+
+    params[name] = param
+
+    return params
+  }, {})
+
+const processSecretDatabases = routes => {
+  let secretDatabases = ['mysql']
+
+  let keysToDeleteFromRoot = new Set()
+
+  secretDatabases.forEach(database => {
+    Object.keys(routes.secret.databases[database]).forEach(apiName => {
+      if (routes.secret.databases[apiName]) {
+        keysToDeleteFromRoot.add(apiName)
+
+        routes.secret.databases[database][apiName] = defaultsDeep(
+          {},
+          routes.secret.databases[apiName],
+          routes.secret.databases[database][apiName]
+        )
+
+        if (routes.secret.databases[database][apiName].paramsToDelete) {
+          routes.secret.databases[database][apiName].paramsToDelete.forEach(
+            key => delete routes.secret.databases[database][apiName].params[key]
+          )
+
+          delete routes.secret.databases[database][apiName].paramsToDelete
+        }
+      }
+    })
+  })
+
+  Array.from(keysToDeleteFromRoot).forEach(
+    key => delete routes.secret.databases[key]
+  )
+}
+
 const generateRoutes = async () => {
   let routes = {}
 
   let endpointFiles = await recursiveReadDir(ENDPOINTS_DIR)
 
   endpointFiles
-    .map(file => ({ route: require(file), ...parsePath(file) }))
-    .map(({ route: { parameters, summary, ...route }, dir, name }) => ({
-      route: {
-        ...route,
-        params: parameters.reduce(
-          (params, { description, name, required, ...param }) => {
-            if (required) param.required = true
-            return Object.assign(params, { [name]: param })
+    .map(file => {
+      let { dir: absDir, name: routeName } = parsePath(file)
+
+      return {
+        absDir,
+        routeName,
+        routeDef: require(file)
+      }
+    })
+    .map(
+      ({ absDir, routeName, routeDef: { parameters = [], ...routeDef } }) => {
+        delete routeDef.summary
+
+        let name = camelCase(routeName)
+        let scope = relativePath(ENDPOINTS_DIR, absDir).replace(/\//g, '.')
+
+        return {
+          route: {
+            ...routeDef,
+            params: processParameters(parameters)
           },
-          {}
-        )
-      },
-      name: camelCase(name),
-      scope: relativePath(ENDPOINTS_DIR, dir).replace(/\//g, '.')
-    }))
+          name,
+          scope
+        }
+      }
+    )
     .forEach(routeObj => {
       set(routes, `${routeObj.scope}.${routeObj.name}`, routeObj.route)
     })
+
+  processSecretDatabases(routes)
 
   Object.entries(routes).forEach(([namespace, routes]) => {
     writeFile(
--- src/plugins/endpoint-methods/injectors/secret.js
+++ src/plugins/endpoint-methods/injectors/secret.js
@@ -1,36 +1,50 @@
+const nestedScopes = {
+  kv: ['v1', 'v2'],
+  databases: ['mysql']
+}
+
+const injectEndpointMethodsForScope = (
+  scopeName,
+  scopeObj,
+  injectionPoint,
+  endpointMethod,
+  core
+) => {
+  injectionPoint[scopeName] = {}
+
+  Object.entries(scopeObj).forEach(([apiName, apiOptions]) => {
+    if (nestedScopes[scopeName] && nestedScopes[scopeName].includes(apiName)) {
+      return injectEndpointMethodsForScope(
+        apiName,
+        apiOptions,
+        injectionPoint[scopeName],
+        endpointMethod,
+        core
+      )
+    }
+
+    let { method, params: paramsSpecs, path: url, ...rest } = apiOptions
+
+    injectionPoint[scopeName][apiName] = endpointMethod.bind(
+      null,
+      core,
+      { method, url, ...rest },
+      paramsSpecs
+    )
+  })
+}
+
 const secretEndpointMethodsInjector = (core, endpointMethod, routes) => {
   core.secret = {}
 
   Object.entries(routes).forEach(([scopeName, scopeObj]) => {
-    core.secret[scopeName] = {}
-
-    if (scopeName === 'kv') {
-      Object.entries(scopeObj).forEach(([versionName, apis]) => {
-        core.secret[scopeName][versionName] = {}
-
-        Object.entries(apis).forEach(([apiName, apiOptions]) => {
-          let { method, params: paramsSpecs, path: url, ...rest } = apiOptions
-
-          core.secret[scopeName][versionName][apiName] = endpointMethod.bind(
-            null,
-            core,
-            { method, url, ...rest },
-            paramsSpecs
-          )
-        })
-      })
-    } else {
-      Object.entries(scopeObj).forEach(([apiName, apiOptions]) => {
-        let { method, params: paramsSpecs, path: url, ...rest } = apiOptions
-
-        core.auth[scopeName][apiName] = endpointMethod.bind(
-          null,
-          core,
-          { method, url, ...rest },
-          paramsSpecs
-        )
-      })
-    }
+    injectEndpointMethodsForScope(
+      scopeName,
+      scopeObj,
+      core.secret,
+      endpointMethod,
+      core
+    )
   })
 }
 
